7af6d14b9cbbd1c73aae8e1d846edf0df0dc95d4
Bruno Vang <buens@outlook.fr>
2024-02-15 22:33:41 +0100

    menu nav responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { elsie } from './components/font';
+import Header from './components/header';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`bg-background ${elsie.className}`}>{children}</body>
+      <body className={`bg-background ${elsie.className}`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
