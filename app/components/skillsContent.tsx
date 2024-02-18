132eac15b8abda80264cd4652d38cae075f84c36
Bruno Vang <buens@outlook.fr>
2024-02-18 17:00:44 +0100

    responsive tablet

--- app/components/skillsContent.tsx
+++ app/components/skillsContent.tsx
@@ -13,7 +13,7 @@ function SkillsContent({ title, skills }: skillContentProps) {
   return (
     <div className="flex flex-col w-full gap-1 bg-menu rounded-2xl lg:p-5 ">
       <h3 className="flex flex-wrap text-2xl font-semibold items-center md:flex md:text-3xl">{title}</h3>
-      <div className="flex lg:flex-col gap-3">
+      <div className="flex md:flex-col gap-3">
         {skills.map((skill, index) => (
           <Skills key={index} src={skill.src} alt={skill.alt} skill={skill.skill} />
         ))}
