f4f7eaadb531f590db97edc69ca938e744fc83ee
Bruno Vang <buens@outlook.fr>
2024-02-20 14:55:57 +0100

    add button contact in section biography

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,14 @@ export default function Home() {
     <main className="flex flex-col min-h-screen p-10 max-w-screen-2xl gap-40">
       <section className="flex flex-col gap-5 justify-center md:justify-between items-center md:flex-row w-full">
         <Banner src="moi.webp" alt="Photo Profil de Bruno" width={500} height={500} />
-        <div className=" flex flex-col gap-8 md:w-7/12 lg:w-1/2">
+        <div className=" flex flex-col gap-8 md:w-7/12 lg:w-2/3">
           <Biography name="Bruno Vang" trade="Develloppeur Front-end" text={mutliText} />
           <div className="grid grid-cols-2 md:grid-cols-5 gap-5 w-full">
             <ButtonContact src="/images/github.svg" buttonLink={{ title: 'GitHub', url: 'https://github.com/Buens18k' }} />
             <ButtonContact src="/images/linkedin.svg" buttonLink={{ title: 'Linkedin', url: 'https://www.linkedin.com/in/bruno-vang/' }} />
             <ButtonContact src="/images/xtwitter.svg" buttonLink={{ title: 'Twitter', url: 'https://twitter.com/_B18k' }} />
             <ButtonContact src="/images/instagram.svg" buttonLink={{ title: 'Instagram', url: 'https://www.instagram.com/vang_bruno18/' }} />
+            <ButtonContact src="/images/mail.svg" buttonLink={{ title: 'Me contacter', url: 'mailto:buens@outlook.fr' }} />
           </div>
         </div>
       </section>
@@ -73,7 +74,7 @@ export default function Home() {
       <section className="w-full">
         <div className="flex flex-col items-center justify-center gap-8 p-10">
           <h5 className="text-3xl font-bold text-text underline">Contact</h5>
-          <div className="grid grid-cols-2 md:grid-cols-5 gap-5 p-20">
+          <div className="grid grid-cols-2 md:grid-cols-5 gap-5 p-16">
             <ButtonContact src="/images/github.svg" buttonLink={{ title: 'GitHub', url: 'https://github.com/Buens18k' }} />
             <ButtonContact src="/images/linkedin.svg" buttonLink={{ title: 'Linkedin', url: 'https://www.linkedin.com/in/bruno-vang/' }} />
             <ButtonContact src="/images/xtwitter.svg" buttonLink={{ title: 'Twitter', url: 'https://twitter.com/_B18k' }} />
