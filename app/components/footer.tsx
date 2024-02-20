4e885bdc1921f596cdfc21543611a611aa6fdb54
Bruno Vang <buens@outlook.fr>
2024-02-20 14:36:46 +0100

    footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <section>
+      <div className="flex justify-center p-10">
+        <p>&copy; 2024 Bruno Vang Tous droits réservés</p>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 // import { Inter } from 'next/font/google';
 import { elsie } from './components/font';
+import Footer from './components/footer';
 import Header from './components/header';
 import './globals.css';
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`bg-background max-w-screen-2xl m-auto text-text ${elsie.className}`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
