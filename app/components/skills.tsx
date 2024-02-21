92a1721ab34e31f502635af1f75bcd02eff3ba74
Bruno Vang <buens@outlook.fr>
2024-02-21 18:09:41 +0100

    review gap

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -8,7 +8,7 @@ interface skillProps {
 
 function Skills({ src, skill, alt }: skillProps) {
   return (
-    <div className="flex text-xl flex-wrap items-center gap-1">
+    <div className="flex text-xl flex-nowrap items-center gap-1">
       <Image className="w-5 h-5" src={`/images/${src}`} alt={alt} width={500} height={500} priority />
       <p>{skill}</p>
     </div>
