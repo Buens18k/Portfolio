1df9635d1837a179177bb7cc41a5063bffa34ace
Bruno Vang <buens@outlook.fr>
2024-02-21 11:37:32 +0100

    min-width button contact

--- app/components/buttonContact.tsx
+++ app/components/buttonContact.tsx
@@ -1,5 +1,6 @@
 import { ButtonLink } from '@/types';
 import Image from 'next/image';
+import Link from 'next/link';
 interface buttonContactProps {
   src: string;
   buttonLink: ButtonLink;
@@ -8,12 +9,12 @@ interface buttonContactProps {
 
 function ButtonContact({ src, buttonLink }: buttonContactProps) {
   return (
-    <a href={buttonLink.url} className="flex justify-center rounded-full bg-menu cursor-pointer min-w-10 p-3 w-full" type="button">
+    <Link href={buttonLink.url} className="flex justify-center rounded-full bg-menu cursor-pointer min-w-28 p-3 w-full" type="button">
       <div className="flex gap-1 lg:gap-3">
         <Image className="w-5 h-5" src={src} alt="logo next" width={500} height={500} priority />
         <p className="text-text flex flex-wrap min-w-full">{buttonLink.title}</p>
       </div>
-    </a>
+    </Link>
   );
 }
 
