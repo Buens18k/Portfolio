da70d9dbfbe66e669f8cde4ddca2195d80740cf5
Bruno Vang <buens@outlook.fr>
2024-02-28 13:47:57 +0100

    imagesMeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import './globals.css';
 // const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://portfolio-theta-seven-36.vercel.app/'),
   title: {
     absolute: '',
     default: 'Portfolio de Bruno Vang',
